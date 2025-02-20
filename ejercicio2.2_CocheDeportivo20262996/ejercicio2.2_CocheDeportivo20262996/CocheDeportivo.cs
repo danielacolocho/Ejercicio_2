0e5bf69b89f91254091ff88f8c78b3d2529e507c
danielacolocho <danielaalejandra21colocho@gmail.com>
2025-02-20 11:21:00 -0600

    Add files via upload

---
+++ ejercicio2.2_CocheDeportivo20262996/ejercicio2.2_CocheDeportivo20262996/CocheDeportivo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2._2_CocheDeportivo20262996
+{
+    internal class CocheDeportivo : Coche
+    {
+        public CocheDeportivo(string marca, string modelo, int velocidadMaxima)
+        : base(marca, modelo, velocidadMaxima)
+        {
+        }
+
+        public void Frena()
+        {
+            VelocidadActual = 0;
+            Console.WriteLine("Velocidad actual: " + VelocidadActual);
+
+        }
+    }
+}
---
+++ ejercicio2.2_CocheDeportivo20262996/ejercicio2.2_CocheDeportivo20262996/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2._2_CocheDeportivo20262996
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

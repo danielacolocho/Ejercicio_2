1a5d214ed6975568c6acae05a98306750fefa855
danielacolocho <danielaalejandra21colocho@gmail.com>
2025-02-20 11:20:02 -0600

    Add files via upload

---
+++ ejercicio0_PersonaNadadora20262996/ejercicio0_PersonaNadadora20262996/Persona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio0_PersonaNadadora20262996
+{
+    public class Persona
+    {
+        public Persona(string ojos, string altura, string peso, string sexo)
+        {
+            this.Ojos = ojos;
+            this.Altura = altura;
+            this.Peso = peso;
+            this.Sexo = sexo;
+        }
+        public string Ojos;
+        public string Altura;
+        public string Peso;
+        public string Sexo;
+        public void Hablar(string texto)
+        {
+            // se muestra la conversacion a decir
+            Console.WriteLine("Voy a decir:" + texto);
+        }
+        public void Comer(double calorias)
+        {
+            // se indica las calorias que se ganan al comer
+            Console.WriteLine("Ganando" + calorias + "Calorias");
+        }
+        public void Correr(double calorias)
+        {
+            // Se indica las calorias que se pierden al correr
+            Console.WriteLine("perdiendo" + calorias + "calorias");
+            this.Peso = Convert.ToString(calorias / 1000);
+        }
+    }
+}
---
+++ ejercicio0_PersonaNadadora20262996/ejercicio0_PersonaNadadora20262996/PersonaNadadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio0_PersonaNadadora20262996
+{
+
+    public class PersonaNadadora:Persona 
+    {
+        public PersonaNadadora(string ojos, string altura, string peso,
+            string sexo) : base(ojos, altura, peso, sexo) { 
+        }
+            public void Nadar()
+        {
+            //La practica de la natacion hara volver al peso inicial
+            Console. WriteLine("Nadando para volver al peso ideal") ;
+            this.Peso = "85";
+            Console.ReadKey();
+         }
+        }
+    }
+
